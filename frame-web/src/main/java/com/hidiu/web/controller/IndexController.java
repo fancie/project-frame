8c8dbab58f3d06a430a93fa7a78d6eb3bb421f1d
1084961@qq.com <liuyingxiang1>
2022-02-07 00:59:02 +0800

    web项目新增ELK

--- frame-web/src/main/java/com/hidiu/web/controller/IndexController.java
+++ frame-web/src/main/java/com/hidiu/web/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.hidiu.feign.es.EsFeignService;
 import com.hidiu.service.TransactionService;
 import com.hidiu.utils.StringUtils;
 import com.hidiu.vo.NewsVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author fancie/1084961@qq.com
  * @date 2022-02-05 12:01:01
  */
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -29,6 +31,7 @@ public class IndexController {
     @ResponseBody
     @RequestMapping("/tx")
     public String tx(){
+        log.info("#--正在访问web项目的/tx方法--#");
         transactionService.testTranaction();
         String random = StringUtils.randomStr(64);
         return  "tx---------" + random;
@@ -37,6 +40,7 @@ public class IndexController {
     @ResponseBody
     @RequestMapping("/r")
     public String r(){
+        log.info("#--正在访问web项目的/r方法--#");
         String random = StringUtils.randomStr(64);
         return  "r----------" + random;
     }
@@ -44,6 +48,7 @@ public class IndexController {
     @ResponseBody
     @RequestMapping("/news/{id}")
     public String getNews(@PathVariable("id") String id){
+        log.info("#--正在访问web项目的/news/id方法--#");
         NewsVo newsVo = esFeignService.findById(id);
         return  "get----------" + newsVo.getTitle();
     }
