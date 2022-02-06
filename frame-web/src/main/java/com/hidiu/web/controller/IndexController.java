85c6194cf4dc007c7e9163a5b3a6f041bee8656b
1084961@qq.com <liuyingxiang1>
2022-02-06 22:28:17 +0800

    fix

--- frame-web/src/main/java/com/hidiu/web/controller/IndexController.java
+++ frame-web/src/main/java/com/hidiu/web/controller/IndexController.java
@@ -4,7 +4,6 @@ import com.hidiu.feign.es.EsFeignService;
 import com.hidiu.service.TransactionService;
 import com.hidiu.utils.StringUtils;
 import com.hidiu.vo.NewsVo;
-import org.bouncycastle.est.ESTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
