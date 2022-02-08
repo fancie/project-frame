6553b9c31f59daf4f3f29dc3712b1873cd657223
1084961@qq.com <liuyingxiang1>
2022-02-08 20:57:59 +0800

    fix

--- frame-web/src/main/java/com/hidiu/web/config/RedisFactoryConfig.java
+++ frame-web/src/main/java/com/hidiu/web/config/RedisFactoryConfig.java
@@ -1,10 +1,8 @@
 package com.hidiu.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
